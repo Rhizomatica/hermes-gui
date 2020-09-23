9ca22ef976f38ee9bfd21b96c4846ce68ac25b12
arianestolfi <arianestolfi@gmail.com>
2020-09-23 11:31:42 -0300

    merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StationsComponent } from './stations/stations.component';
@@ -9,11 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { MessagesComponent } from './messages/messages.component';
 import { MessageDetailComponent } from './message-detail/message-detail.component';
 import { AlertsComponent } from './alerts/alerts.component';
-<<<<<<< HEAD
 import { SysadminComponent } from './sysadmin/sysadmin.component'; 
 
-=======
->>>>>>> 7f4bcdfc3717d8ada75c012b250ea7c1a7f90f0a
 
 @NgModule({
   declarations: [
