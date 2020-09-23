7057a556b8f53a59260456bb5dc1dc46bf81d2c6
arianestolfi <arianestolfi@gmail.com>
2020-09-23 11:34:12 -0300

    merge

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { MessagesComponent } from "./messages/messages.component";
 import { SysadminComponent } from './sysadmin/sysadmin.component'; 
 import { StationsComponent } from './stations/stations.component'; 
 
-=======
-import { MessagesComponent } from './messages/messages.component';
->>>>>>> 7f4bcdfc3717d8ada75c012b250ea7c1a7f90f0a
 
 const routes: Routes = [
   { path: 'messages', component: MessagesComponent},
