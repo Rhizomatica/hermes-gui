d98e13b10dffa17cf7a3216d81c0c6c1858890f5
Carlos Paulino <chgp@riseup.net>
2020-10-27 10:14:09 -0300

    get stations from another component

--- src/app/messagecompose/messagecompose.component.ts
+++ src/app/messagecompose/messagecompose.component.ts
@@ -3,20 +3,23 @@ import { HmheaderComponent } from '../hmheader/hmheader.component';
 import { Observable, of, throwError } from 'rxjs';
 
 import { Station } from '../station';
-import { StationsComponent } from '../stations/stations.component';
 import { StationService } from '../station.service';
-
+//import { StationsComponent } from '../stations/stations.component';
 
 @Component({
   selector: 'app-messagecompose',
   templateUrl: './messagecompose.component.html',
   styleUrls: ['./messagecompose.component.less']
 })
+
+
 export class MessagecomposeComponent implements OnInit {
-
-  constructor() { }
-
+  stations: Station[];
+  constructor(private stationService: StationService) {}
+  
   ngOnInit(): void {
+    this.stationService.getStations()
+    .subscribe(stations =>  this.stations = stations)
   }
 
 }
