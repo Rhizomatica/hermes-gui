c40009629638f7f34d87896d9d9947b131ad4137
arianestolfi <arianestolfi@gmail.com>
2021-02-04 17:46:44 -0300

    er exist directive

---
+++ src/app/user-exist.directive.spec.ts
@@ -0,0 +1,8 @@
+import { UserExistDirective } from './user-exist.directive';
+
+describe('UserExistDirective', () => {
+  it('should create an instance', () => {
+    const directive = new UserExistDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/user-exist.directive.ts
@@ -0,0 +1,60 @@
+
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, FormGroup, NG_VALIDATORS, ValidationErrors, Validator, ValidatorFn } from '@angular/forms';
+import { User } from './user';
+import { UserService } from './_services/user.service';
+
+
+
+export function compareUsername(userList: any): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    let alreadyExist = false;
+    for (let user of userList) {
+      if (user.email === control.value) {
+        alreadyExist = true;
+      }
+    }
+    return alreadyExist ? { Username: { value: control.value } } : null;
+  };
+}
+
+//stackoverflow.com/questions/51114853/angular-6-template-driven-form-check-if-email-already-exist
+
+@Directive({
+  selector: '[appUserExist]',
+  exportAs: 'userExist',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: UserExistDirective,
+      multi: true
+    }
+  ]
+})
+export class UserExistDirective implements Validator {
+  @Input('appUserExist') userFound: string; 
+  userList: User[];
+
+
+  constructor(private userService: UserService) {}
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (res: any) => {
+        this.userList = res;
+      },
+      (err) => {
+        this.userList = err;
+      }
+    );
+  }
+
+  validate(control: AbstractControl): { [key: string]: any } | null {
+    return this.userList ? null : compareUsername(this.userList)(control);
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+}
