5005c3793f2ce2975aeb2a358c51214692e2b39d
arianestolfi <arianestolfi@gmail.com>
2021-02-07 18:08:31 -0300

    no server error

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -1,8 +1,8 @@
 import { HttpHeaders} from '@angular/common/http';
 export class GlobalConstants {
 
-    public static apiURL = 'http://pu2uit.hermes.radio:1011/api';
-    // public static apiURL = 'http://localhost:8000';
+     public static apiURL = 'http://pu2uit.hermes.radio:1011/api';
+    //public static apiURL = 'http://localhost:8000';
     // TODO this must be fix to localhost in stations!
     // public static apiURL = 'http://localhost/api';
 
