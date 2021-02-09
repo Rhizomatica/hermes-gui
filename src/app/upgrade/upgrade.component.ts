12148c25fec462a6a8108fd24e20b32d35896bfe
arianestolfi <arianestolfi@gmail.com>
2021-02-09 20:01:24 -0300

    admin session

--- src/app/upgrade/upgrade.component.ts
+++ src/app/upgrade/upgrade.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpgradeComponent implements OnInit {
 
+restore: boolean = false;
+restoreConf: boolean = false;
+
+  restoreAlert() {
+  this.restore =true;
+  }
+
+  restoreConfirm() {
+    this.restoreConf = true;
+  }
+
+  cancelRestore() {
+    this.restore = false;
+    this.restoreConf = false;
+  }
+  
+
   constructor() { }
 
   ngOnInit(): void {
