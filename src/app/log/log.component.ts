b441939dc52f7642219f0016ed1b968fc0310b2b
arianestolfi <arianestolfi@gmail.com>
2021-10-22 19:34:02 -0300

    adding log page logic

--- src/app/log/log.component.ts
+++ src/app/log/log.component.ts
@@ -7,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogComponent implements OnInit {
 
+  uLog: boolean = false;
+  eLog: boolean = false
+
   constructor() {
   }
 
+  showUucpLog() {
+    this.uLog = true;
+    this.eLog = false;
+  }
+
+  showEmailLog() {
+    this.uLog = false;
+    this.eLog = true;
+  }
+
+
+  closeLogs() {
+    this.uLog = false;
+    this.eLog = false;
+  }
+
+getLogs() {
+  console.log('logs');
+}
+
+
   ngOnInit(): void {
+    this.getLogs();
   }
 
 }
