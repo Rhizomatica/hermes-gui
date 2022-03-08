ad2a6654240275067d31ad09cc77b218e5faeee4
arianestolfi <arianestolfi@gmail.com>
2022-03-08 17:36:27 -0300

    changig typography

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -3,12 +3,12 @@ export class GlobalConstants {
 
   // TODO this must be /api to deploy!
   // public static apiURL = 'http://10.8.0.18/api'; // k4
-  // public static apiURL = 'http://10.8.0.2/api'; // k4
+  public static apiURL = 'http://10.8.0.2/api'; // k4
   // public static apiURL = 'http://187.20.47.153:8080/api'; // k4
   // public static apiURL = 'http://10.8.0.3/api'; // k6
   // public static apiURL = 'http://192.168.0.4/api';
   // public static apiURL = 'http://192.168.0.105/api';
-  public static apiURL = '/api'; // default to deploy
+  // public static apiURL = '/api'; // default to deploy
 
     public static httpOptions = {
         headers: new HttpHeaders({
