9e9f3d1079ed1c6bd45917ea78428015bd583735
arianestolfi <arianestolfi@gmail.com>
2022-03-11 15:32:34 -0300

    adding email to landing page

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -8,7 +8,7 @@ export class GlobalConstants {
   // public static apiURL = 'http://10.8.0.3/api'; // k6
   // public static apiURL = 'http://192.168.0.4/api';
   // public static apiURL = 'http://192.168.0.105/api';
-  public static apiURL = '/api'; // default to deploy
+ public static apiURL = '/api'; // default to deploy
 
     public static httpOptions = {
         headers: new HttpHeaders({
