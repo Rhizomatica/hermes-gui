3184b7d24c633ebd4b28a65cc699a0b58fdb1cc9
mtsthibau <mtsthibau@gmail.com>
2022-03-16 17:31:40 -0300

    ignore .env file / enviroments default

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-   appName: 'a-HERMES',
-   appKey: '',
-   appDebug: 'true',
-   appLogLevel: 'debug',
-   appUrl: 'http://localhost:4200/',
-   apiBaseUrl: '',
-   apiUrl: 'http://10.8.0.3/api',
-   nodeEnv: 'development',
-   production: 'false'
+  production: true
 };
