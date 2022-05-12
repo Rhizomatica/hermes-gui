057f0711c1d699cb5f5edfc7f409074fc3814e98
mtsthibau <mtsthibau@gmail.com>
2022-05-12 20:13:32 -0300

    FIX | NetAdmin System === undefined

--- src/app/components/admin/netadmin/netadmin.component.ts
+++ src/app/components/admin/netadmin/netadmin.component.ts
@@ -1,37 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../_services/api.service';
 
-
 @Component({
   selector: 'app-netadmin',
   templateUrl: './netadmin.component.html',
   styleUrls: ['./netadmin.component.less']
 })
+
 export class NetadminComponent implements OnInit {
 
-  error: any;
-  system: any;
+  error: any
+  system: any
+  errorAlert = false
 
   constructor(
     private apiService: ApiService
-
   ) { }
 
-  getSystemStatus(): void{
+  getSystemStatus(): void {
     this.apiService.getStatus().subscribe(
       (res: any) => {
-        this.system = res;
-        return res;
+        this.system = res
+        return res
       },
       (err) => {
-        this.error = err;
+        this.error = err
+        this.errorAlert = true
       }
-    );
+    )
+  }
+
+  closeError() {
+    this.errorAlert = false
   }
 
   ngOnInit(): void {
-    this.getSystemStatus();
-    // console.log(this.system);
+    this.getSystemStatus()
   }
-
 }
