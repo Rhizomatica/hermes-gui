9468155ac80acf5a8341bfdd990c472f78cfc9e7
mtsthibau <mtsthibau@gmail.com>
2022-11-23 17:54:39 -0300

    FIX | TDD (LoginComponnent test NgForm)

--- src/app/components/public/login/login.component.spec.ts
+++ src/app/components/public/login/login.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientModule } from '@angular/common/http';
-
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -13,7 +13,9 @@ describe('LoginComponent', () => {
       declarations: [LoginComponent],
       imports: [
         RouterTestingModule,
-        HttpClientModule
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule
       ]
     })
       .compileComponents();
