24f7bc9f191fdc5e4e6b3596e44fd55d37cbf419
mtsthibau <mtsthibau@gmail.com>
2023-04-28 15:31:48 +1000

    FEAT | GUI - New landing (nav styles, color theme)

--- src/app/components/public/landing/landing.component.ts
+++ src/app/components/public/landing/landing.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 import { ApiService } from 'src/app/_services/api.service';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 import { UtilsService } from 'src/app/_services/utils.service';
+import { DarkModeService, DARK_MODE_OPTIONS } from 'angular-dark-mode';
 import { User } from 'src/app/interfaces/user';
 
 @Component({
@@ -10,12 +12,13 @@ import { User } from 'src/app/interfaces/user';
   styleUrls: ['./landing.component.less']
 })
 export class LandingComponent implements OnInit {
+  [x: string]: any;
 
 
   constructor(
     private authenticationService: AuthenticationService,
     private apiService: ApiService,
-    private utils: UtilsService
+    private darkModeService: DarkModeService,
   ) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     if (this.currentUser)
@@ -24,9 +27,30 @@ export class LandingComponent implements OnInit {
 
   currentUser: User = null
   admin: boolean = false
+  darkMode$: Observable<boolean> = this.darkModeService.darkMode$;
+  currentTheme = 'light'
 
   ngOnInit(): void {
+    this.currentTheme = JSON.parse(localStorage.getItem('dark-mode')).darkMode == true ? 'dark' : 'light'
+  }
 
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+    this.currentUser = null;
+  }
 
+  toggle(): void {
+    if (this.currentTheme == 'light') {
+      this.currentTheme = 'dark'
+      this.darkModeService.toggle();
+      return
+    }
+
+    if (this.currentTheme == 'dark') {
+      this.currentTheme = 'light'
+      this.darkModeService.toggle();
+      return
+    }
   }
 }
