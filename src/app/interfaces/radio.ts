e3aad6eb35106b913d2df12a711dc67566bec1e9
mtsthibau <mtsthibau@gmail.com>
2023-07-18 19:55:11 +1000

    FEAT | GUI - Websocket retrieving different type of data

--- src/app/interfaces/radio.ts
+++ src/app/interfaces/radio.ts
@@ -1,6 +1,18 @@
 export interface Radio {
-    irxs: Number,
-    freq: Number,
-    mode: String,
-    protection: String
-  }
+  type: String,
+  irxs: Number,
+  freq: Number,
+  mode: String,
+  protection: String,
+  tx: Boolean,
+  rx: Boolean,
+  led: Boolean,
+  fwd_raw: Number,
+  fwd_volts: Number,
+  fwd_watts: Number,
+  swr: Number,
+  ref_raw: Number,
+  ref_volts: Number,
+  ref_watts: Number,
+  connection: Boolean
+}
