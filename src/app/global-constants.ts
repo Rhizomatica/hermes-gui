0667853f32ea4ff3179b03e00610b579d00f3551
mtsthibau <mtsthibau@gmail.com>
2023-08-07 19:26:01 +1000

    FIX | GUI - Removing flags from help page

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -6,8 +6,8 @@ export class GlobalConstants {
   public static version = environment.version
   public static producion = environment.production
   public static local = environment.local
-  public static apiURL = environment.local ? 'https://10.8.0.129/api' : `https://${self.location.hostname + '/api'}`
-  public static webSocketUrl = environment.local ? 'wss://10.8.0.129:8080/websocket' : `wss://${self.location.hostname + ':8080/websocket'}`
+  public static apiURL = environment.local ? 'https://10.8.0.127/api' : `https://${self.location.hostname + '/api'}`
+  public static webSocketUrl = environment.local ? 'wss://10.8.0.127:8080/websocket' : `wss://${self.location.hostname + ':8080/websocket'}`
   public static hasGPS = environment.hasGPS
   public static domain = environment.domain
   public static gateway = environment.gateway
