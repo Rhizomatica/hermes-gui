77a5072d5b60395790216d62bfd05f201b8ce540
mtsthibau <mtsthibau@gmail.com>
2023-08-08 15:10:20 +1000

    FEAT | GUI - Remove unused items

--- src/app/_services/wifi-manager.service.ts
+++ src/app/_services/wifi-manager.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError, of } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import { GlobalConstants } from '../global-constants';
 
