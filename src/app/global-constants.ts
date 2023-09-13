10e0d07b756c26964228f54c052e053d8a9a51c8
mtsthibau <mtsthibau@gmail.com>
2023-09-13 15:44:29 +0800

    FEAT | GUI - Improove serverIP global_constants

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -2,14 +2,15 @@ import { HttpHeaders } from '@angular/common/http';
 import { environment } from '../environments/environment';
 export class GlobalConstants {
 
-  public static producion = environment.production
   public static local = environment.local
-  public static apiURL = environment.local ? 'https://10.8.0.129/api' : `https://${self.location.hostname + '/api'}`
-  public static webSocketUrl = environment.local ? 'wss://10.8.0.129:8080/websocket' : `wss://${self.location.hostname + ':8080/websocket'}`
-  public static hasGPS = environment.hasGPS
+  public static producion = environment.production
+  public static serverIP = '10.8.0.128'
+  public static apiURL = environment.local ? `https://${environment.serverIP}/api` : `https://${self.location.hostname}/api`
+  public static webSocketUrl = environment.local ? `wss://${environment.serverIP}/websocket` : `wss://${self.location.hostname + ':8080/websocket'}`
   public static domain = environment.domain
   public static gateway = environment.gateway
   public static bitx = environment.bitx
+  public static hasGPS = environment.hasGPS
   public static generalLogin = environment.generalLogin
   public static emergencyEmail = environment.emergencyEmail
 
