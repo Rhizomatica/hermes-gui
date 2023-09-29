57b63532dfe57174e679be5404beb451577d5dcd
mtsthibau <mtsthibau@gmail.com>
2023-09-29 14:46:42 +0700

    FIX | GUI - Translation input search error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     local: true,
-    production: true,
+    production: false,
     serverIP: '10.8.0.128',
     domain: 'estacao.hermes.radio',
     gateway: true,
     bitx: 'S',
     hasGPS: false,
     generalLogin: true,
-    emergencyEmail: 'assistancesap@invisiblechildren.com'
+    emergencyEmail: 'help@hermes.radio'
 };
