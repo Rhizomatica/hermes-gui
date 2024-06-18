df65596014bc91f858df73e40a46b959d0e888c5
mtsthibau <mtsthibau@gmail.com>
2024-06-18 10:44:22 +0700

    FEAT | GUI - Format phone number list

--- src/app/components/public/sms/sms.component.ts
+++ src/app/components/public/sms/sms.component.ts
@@ -19,7 +19,9 @@ export class SMSComponent implements OnInit {
   error: string
   errorAlert: boolean = false
   message: SMSMessage
+  message1: SMSMessage
   messages: SMSMessage[]
+  filteredMessages: SMSMessage[] = []
 
   constructor(
     private authenticationService: AuthenticationService,
@@ -46,37 +48,54 @@ export class SMSComponent implements OnInit {
     this.messages.push(this.message)
     this.messages.push(this.message)
 
-    this.smsMessageService.getMessages().subscribe(
-      (res: any) => {
-        this.messages = res.filter(a => a.phoneNumber);
-        // this.filterSameNumbers()
-      },
-      (err) => {
-        this.error = err;
-        this.errorAlert = true;
-        this.loading = false;
-      }
-    );
+    this.message1.id = 2
+    this.message1.text = "Hello two"
+    this.message1.phoneNumber = "+5531998874554"
+    this.message1.type = 0 //received
+    this.message1.date = this.utilsService.formatDate(new Date())
+    this.messages.push(this.message1)
+
+
+    // this.smsMessageService.getMessages().subscribe(
+    //   (res: any) => {
+    //     this.messages = res.filter(a => a.phoneNumber);
+    //     this.filterMessages()
+    //   },
+    //   (err) => {
+    //     this.error = err;
+    //     this.errorAlert = true;
+    //     this.loading = false;
+    //   }
+    // );
+
+    this.filterMessages()
   }
 
-  // filterSameNumbers() { //TODO - FINISH **DONT FORGET THE datetime
-  //   this.phonesList = [];
+  filterMessages() {
+    for (var i in this.messages) {
 
-  //   for (var i in this.messages ) {
-  //     if (getNumber.indexOf(this.messages[i]) < 0)    //See if the number wasn't found already
-  //     {
-  //       getNumber.push(this.messages[i]);
-  //     }
-  //     else {
-  //       //This number was found before. Do nothing!
-  //     }
-  //   }
-  // }
+      if(!this.filteredMessages || this.filteredMessages.length == 0){
+        this.filteredMessages.push(this.messages[i]);
+        continue
+      }
 
+      if (this.filteredMessages.map(e => e.phoneNumber).indexOf(this.messages[i].phoneNumber) < 0) {
+        this.filteredMessages.push(this.messages[i]);
+      }
+    }
+  }
 
   ngOnInit(): void {
 
     this.message = {
+      id: 0,
+      text: "",
+      phoneNumber: "",
+      type: 0,
+      date: null,
+    }
+
+    this.message1 = {
       id: 0,
       text: "",
       phoneNumber: "",
