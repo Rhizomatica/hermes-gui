402188d58857ca5d794cbc78c4e89dc61341f320
mtsthibau <mtsthibau@gmail.com>
2024-07-15 12:20:01 +0900

    FEAT | GUI - Preparing operator panel

--- src/app/components/public/operator/operator.component.ts
+++ src/app/components/public/operator/operator.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 import { User } from 'src/app/interfaces/user';
+import { RadioService } from '../../../_services/radio.service';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'operator',
@@ -12,16 +14,41 @@ export class OperatorComponent implements OnInit {
 
   currentUser: User = null
   admin: boolean = false
+  systemData: object
+  loading: boolean = false
+  error: Error
+  poolSystemData: Subscription
 
   constructor(
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private radioService: RadioService
   ) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     if (this.currentUser)
       this.admin = this.currentUser.admin
   }
 
+  getSystemData() {
+    // this.loading = true
+    // this.radioService.getSystemData().subscribe(
+    //   (res: any) => {
+    //     if (res)
+    //       this.systemData = res
+
+    //     this.loading = false
+    //   },
+    //   (err) => {
+    //     this.error = err;
+    //     this.loading = false
+    //   }
+    // );
+  }
+
   ngOnInit(): void {
-  
+    this.getSystemData()
+
+    this.poolSystemData = interval(10000).subscribe((val) => {
+      this.getSystemData()
+    });
   }
 }
