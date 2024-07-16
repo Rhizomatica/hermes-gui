f0426d7828d497ddf939f2b762b75d23efe70cbc
mtsthibau <mtsthibau@gmail.com>
2024-07-16 12:59:01 +0900

    FEAT | GUI - Get enabled schedules (operator panel)

--- src/app/components/public/operator/operator.component.ts
+++ src/app/components/public/operator/operator.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
 import { User } from 'src/app/interfaces/user';
-import { RadioService } from '../../../_services/radio.service';
 import { Subscription, interval } from 'rxjs';
 import { SharedService } from 'src/app/_services/shared.service';
 import { ApiService } from 'src/app/_services/api.service';
@@ -24,36 +23,36 @@ export class OperatorComponent implements OnInit {
   radio: any = []
   diskSpace: string = '0'
   gpsStatus: boolean
+  activeSchedule: boolean = false
 
   constructor(
     private authenticationService: AuthenticationService,
-    private radioService: RadioService,
     private sharedService: SharedService,
     private apiService: ApiService,
-    private gpsService: GPSService
-
-
-
-  ) {
+    private gpsService: GPSService) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
     if (this.currentUser)
       this.admin = this.currentUser.admin
   }
 
-  getSystemData() {
-    // this.loading = true
-    // this.radioService.getSystemData().subscribe(
-    //   (res: any) => {
-    //     if (res)
-    //       this.systemData = res
+  public getSchedules(): void {
+    this.loading = true
+    this.apiService.getSchedules().subscribe(
+      (res: any) => {
 
-    //     this.loading = false
-    //   },
-    //   (err) => {
-    //     this.error = err;
-    //     this.loading = false
-    //   }
-    // );
+        var enabledSchedules = res.filter((a) => { return a.enable == 1 })
+
+        if (enabledSchedules.length > 0)
+          this.activeSchedule = true
+
+        this.loading = false
+      },
+      (err) => {
+        this.error = err;
+        // this.errorAlert = true
+        this.loading = false
+      }
+    );
   }
 
   getGPSStatus(): void {
@@ -77,7 +76,7 @@ export class OperatorComponent implements OnInit {
     this.apiService.getStatus().subscribe(
       (res: any) => {
 
-        this.diskSpace = (res.diskfree /1024 /1024).toFixed(3)
+        this.diskSpace = (res.diskfree / 1024 / 1024).toFixed(3)
         this.loading = false
       },
       (err) => {
@@ -90,12 +89,12 @@ export class OperatorComponent implements OnInit {
   ngOnInit(): void {
     this.radio = this.sharedService.radioObj.value
 
-    // this.getSchedules()
+    this.getSchedules()
     this.getGPSStatus()
     this.getSystemStatus()
 
     this.poolSystemData = interval(10000).subscribe((val) => {
-      this.getSystemData()
+      // this.getSystemData()
     });
 
 
@@ -107,7 +106,7 @@ export class OperatorComponent implements OnInit {
     // Successful Transmission
     // Re-transmissions count
     //
-    
+
     // ONE API CALL
     // Scheduled transmission
     // GPS Status
