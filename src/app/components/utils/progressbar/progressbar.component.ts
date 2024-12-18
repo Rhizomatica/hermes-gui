1074c433d0b30533f12053053e10ac130f03f044
Matheus Thibau <mtsthibau@gmail.com>
2024-12-18 17:53:32 -0300

    FIX | GUI - Fixing Progress bar component

--- src/app/components/utils/progressbar/progressbar.component.ts
+++ src/app/components/utils/progressbar/progressbar.component.ts
@@ -8,10 +8,10 @@ import { Component, Input, OnChanges } from '@angular/core';
 export class ProgressBarComponent implements OnChanges {
 
   constructor() {
-    this.value = true
+    this.value = null
   }
 
-  @Input() value: boolean
+  @Input() value: string
 
   ngOnChanges(change) {
     change && change.value.currentValue != change.value.previousValue ? this.value = change.value.currentValue : null
