66ce8b3c7e6302e328525ad313e2b0fe3b8dfd3b
Matheus Thibau <mtsthibau@gmail.com>
2025-04-01 14:36:16 -0300

    FIX | GUI - Updating karma configurations (build actions)

--- karma.conf.js
+++ karma.conf.js
@@ -26,9 +26,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
