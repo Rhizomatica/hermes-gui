0928641f842895c3abdaeb01c4fb4e2a69464b6c
Matheus Thibau <mtsthibau@gmail.com>
2025-06-11 23:22:58 -0300

    FEAT | GUI - Get env variables from github (action build) #50

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: false,
     // browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: false
   });
 };
